1d735f65356e8ad76602b352407c00b76424ab16
Duksu Kim <bluekds@koreatech.ac.kr>
2020-11-22 20:46:05 +0900

    update

--- Lab/LvUp/LvUp.c
+++ Lab/LvUp/LvUp.c
@@ -6,11 +6,38 @@
 #define MAX_NAME_SIZE 255
 #define LEVEL_UP_RATE 1.1f;
 
-enum Charater_Type { };
+enum Charater_Type { WARRIOR = 0b00, WIZARD = 0b01, ARCHER = 0b10, PRIEST = 0b11 };
+
 typedef unsigned int UINT;
 
 typedef struct {
-	// define this structure
+	char name[MAX_NAME_SIZE];
+
+	union {
+		UINT ID;
+		struct {
+			UINT ID_dummy : 30;
+			UINT type : 2;
+		};
+	};
+
+	UINT Lv;
+
+	union {
+		UINT hp;
+		struct {
+			UINT hp_postFix : 2;
+			UINT hp_dummy : 30;
+		};
+	};
+
+	union {
+		UINT ability;
+		UINT st; // stamina
+		UINT mp; // magic power
+		UINT ar; // arrow
+		UINT pr; // prayer
+	};
 } character;
 
 void printCharInfo(character* _char);
@@ -38,15 +65,54 @@ int main() {
 }
 
 int setCharInfo(character* _char) {
-	// write your code here
-	return _char->ID;
+	UINT type;
+	printf("Select Type: 0.Warrior 1.Wizard 2.Archer 3.Priest : ");
+	scanf("%d", &type);
+
+	if (type > 3) return -1;
+
+	// ID
+	_char->ID = ID_base;
+	_char->type = type;
+	ID_base++;
+
+	// Lv
+	_char->Lv = 1;
+
+	// Abilities
+	printf("Enter [name] [HP] [ST|MP|AR|PR] : ");
+	scanf("%s %d %d", _char->name, &_char->hp, &_char->ability);
+
+	_char->hp_postFix = type;
+
+	return 0;
 }
 
-int levelUp(character* _char){
-	// write your code here
-	return _char->ID;
+int levelUp(character* _char) {
+	_char->Lv++;
+	_char->hp = _char->hp * LEVEL_UP_RATE;
+	_char->hp_postFix = _char->type;
+
+	_char->ability = _char->ability * LEVEL_UP_RATE;
+
+	return 0;
 }
 
 void printCharInfo(character* _char) {
-	// write your code here
+	printf("[Name] %s [ID] %u ", _char->name, _char->ID);
+
+	switch (_char->type) {
+	case WARRIOR:
+		printf("[Warrior, Lv.%d] [HP] %d [ST] %d\n", _char->Lv, _char->hp, _char->st);
+		break;
+	case WIZARD:
+		printf("[Wizard, Lv.%d] [HP] %d [MP] %d\n", _char->Lv, _char->hp, _char->mp);
+		break;
+	case ARCHER:
+		printf("[Archer, Lv.%d] [HP] %d [AR] %d\n", _char->Lv, _char->hp, _char->ar);
+		break;
+	case PRIEST:
+		printf("[Priest, Lv.%d] [HP] %d [PR] %d\n", _char->Lv, _char->hp, _char->pr);
+		break;
+	}
 }
